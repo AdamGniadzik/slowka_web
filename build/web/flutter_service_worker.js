a66d601a35b4af7b40a4f9e8c9120a2ebdc6d163
AdamGniadzik <216759@edu.p.lodz.pl>
2020-08-30 15:17:21 +0200

    WEB 1.0

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "de85449460d0df1242fa5150a8035622",
 "/": "de85449460d0df1242fa5150a8035622",
-"main.dart.js": "9d164ddd94e750bdab176722832efea7",
+"main.dart.js": "0c272783d43279c1d9676898dba987df",
 "manifest.json": "d0f144b7a603ac0b76f453048ede48d3"
 };
 
